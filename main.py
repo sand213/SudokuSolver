4355a4c7bbb32a8c86b1a760b168cdea19f65e39
Seb Hodge <sand213@aucklanduni.ac.nz>
2021-07-12 20:54:06 +1200

    Created Board Generator

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is the start of the Sudoku Solver program
+# SudokuSolver Functions
+import numpy as np
+def main():
+    puzzle = CreateBoard()
+    return
+    # use this function to execute the code
+
+def CreateBoard():
+    board = np.zeros((9,9))
+    num = np.random.randint(9)
+    return board
+
+if __name__ == "__main__":
+    main()
