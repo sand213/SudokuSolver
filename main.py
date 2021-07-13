2747ea42a713a5d5c0ab3eeff356e51c69f4a169
Seb Hodge <sand213@aucklanduni.ac.nz>
2021-07-13 20:56:43 +1200

    Finished function CreateBoard

--- main.py
+++ main.py
@@ -1,40 +1,62 @@
 # This is the start of the Sudoku Solver program
 # SudokuSolver Functions
 import numpy as np
+import math 
+
 def main():
     puzzle = CreateBoard()
-    print(puzzle)
+    displayPuzzle()
     return
     # use this function to execute the code
 
+def displayPuzzle():
+    return
+
 def CreateBoard():
     # generates the starting sudoku board 
-    board = np.zeros((10,10))
+    board = np.zeros((11,11))
     # adds borders to the board
-    for i in range(10):
-        for j in range(10):
-            if i == 0 or i == 9:
+    for i in range(11):
+        for j in range(11):
+            if i == 0 or i == 10:
                 board[i][j] = 10
-            if j == 0 or j == 9:
+            if j == 0 or j == 10:
                 board[i][j] = 10
     # gets a random position on the board to place a number between 1 and 9
-    #row = np.random.randint(low = 1, high = 8)
-    #col = np.random.randint(low = 1, high = 8)
-    row = 1
-    col = 1
-    board[1][3] = 1
-    board[2][1] = 1
-    num = 1
+    amount = int(input("Enter how many numbers you want to start of with: "))
+    pos = []
+    while amount > 0:
+        row = np.random.randint(low = 1, high = 9)
+        col = np.random.randint(low = 1, high = 9)
+        num = np.random.randint(9)
+        match = False
+        for pair in pos:
+            if pair == [row,col]:
+                row = 0
+                col = 0
+                match = True
+                num = 0
+        if match == False:
+            pos.append([row,col])
+        
     
     # check if we can place the number in this position
-    for i in range(1,9):
-        if board[row][i] == num:
-            num = 0
-        if board[i][col] == num:
-            num = 0
+        rowpos = math.ceil(row / 3)
+        colpos = math.ceil(col / 3)
+        for j in range((rowpos*3), ((rowpos*3) - 3), -1):
+            for k in range((colpos*3),((colpos*3) - 3), -1):
+                if board[j][k] == num:
+                    num = 0
+        for i in range(1,9):
+            if board[row][i] == num:
+                num = 0
+            if board[i][col] == num:
+                num = 0
+        
 
-
-    board[row][col] = num
+        if (match is False) and num != 0:
+            board[row][col] = num
+            amount = amount - 1
     return board
 
 if __name__ == "__main__":
